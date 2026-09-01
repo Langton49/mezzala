7a92a4882d1d79de57daaa599eacad18d0cc3a93
Langton49 <munashemukweya2022@gmail.com>
2026-09-01 12:22:52 -0500

    first commit

---
+++ config/leagues.py
@@ -0,0 +1,85 @@
+LEAGUES = {
+    "epl": {
+        "bzzorio_id": 1,
+        "name": "Premier League",
+        "country": "England"
+        
+    },
+    "la_liga": {
+        "bzzorio_id": 3,
+        "name": "La Liga",
+        "country": "Spain"
+    },
+    "bundesliga": {
+        "bzzorio_id": 5,
+        "name": "Bundesliga",
+        "country": "Germany"
+    },
+    "serie_a": {
+        "bzzorio_id": 4,
+        "name": "Serie A",
+        "country": "Italy"
+    },
+    "ligue_1": {
+        "bzzorio_id": 6,
+        "name": "Ligue 1",
+        "country": "France"
+    },
+    "champoinship": {
+        "bzzorio_id": 12,
+        "name": "Championship",
+        "country": "England"
+    },
+    "fa_cup": {
+        "bzzorio_id": 39,
+        "name": "FA Cup",
+        "country": "England"
+    },
+    "cdr": {
+        "bzzorio_id": 41,
+        "name": "Copa del Rey",
+        "country": "Spain"
+    },
+    
+    "coppa_italia": {
+        "bzzorio_id": 42,
+        "name": "Coppa Italia",
+        "country": "Italy"
+    },
+    
+    "dfb_pokal": {
+        "bzzorio_id": 43,
+        "name": "DFB Pokal",
+        "country": "Germany"
+    },
+    
+    "coupe_de_france": {
+        "bzzorio_id": 44,
+        "name": "Coupe de France",
+        "country": "France"
+    },
+    
+    "ucl": {
+        "bzzorio_id": 7,
+        "name": "Champions League",
+        "country": "Europe"
+    },
+    
+    "uel": {
+        "bzzorio_id": 8,
+        "name": "Europa League",
+        "country": "Europe"
+    },
+    
+    "unl": {
+        "bzzorio_id": 64,
+        "name": "UEFA Nations League",
+        "country": "Europe"
+    },
+    
+    "uecl": {
+        "bzzorio_id": 83,
+        "name": "Conference League",
+        "country": "Europe"
+    }
+}
