10630ee7300e0e7343ad75350145c7819931110f
Langton49 <munashemukweya2022@gmail.com>
2026-09-02 19:15:05 -0500

    Finished initial analysis of data sources in data_exploration notebook

---
+++ config/news_feeds.py
@@ -0,0 +1,7 @@
+NEWS_FEEDS = {
+    "BBC": "https://feeds.bbci.co.uk/sport/football/rss.xml",
+    "SKY Sports": "https://www.skysports.com/rss/12040",
+    "CBS Sports": "https://www.cbssports.com/rss/headlines/soccer/",
+    "ESPN Sports": "https://www.espn.com/espn/rss/soccer/news",
+    "FOX Sports": "https://api.foxsports.com/v2/content/optimized-rss?partnerKey=MB0Wehpmuj2lUhuRhQaafhBjAJqaPU244mlTDK1i&size=30&tags=fs%2Fsoccer"   
+}
