720036282aa780aef77a764167715c1710f5717f
Langton49 <munashemukweya2022@gmail.com>
2026-09-05 03:14:31 -0500

    SETUP finished setting up docker compose file for starting up postgres and redis

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     bzzorio_api_key: str
     bzzorio_base_url: str
+    db_url: str
+    redis_url: str
     model_config = SettingsConfigDict(env_file=".env.local")
 
 settings = Settings()
